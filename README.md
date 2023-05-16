52205bb574d00180417ff3ca888b13007e6156e0
Dimon <yoh60438@gmail.com>
2023-05-16 23:26:46 +0700

    introduction

--- README.md
+++ README.md
@@ -2,5 +2,9 @@ Hello!
 
 I study Node.js here from https://youtu.be/3aGSqasVPsI
 
+```
+npm init 
+```
+to init project
 Best wishes,
 Dmitry Utev.
---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk')
+const text = require('./data.js')
+
+console.log(chalk.blue(text));
+
+console.log(__dirname) //system variable
+console.log(__filename)
